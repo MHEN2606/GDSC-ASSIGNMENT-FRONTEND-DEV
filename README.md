92796b303bf1f61250e1b956b028ff42912fa4b7
Matthew Mahendra <matthew.mahendra@gmail.com>
2021-10-26 12:10:48 +0700

    Added rating to data

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 ## Front Page Design
 ![screenshot](./img/frontpage.PNG)
+View for the frontpage. Users can access all movies and see the detail button to click it.
+
 ## Movie Detail Page
 ![screenshot](./img/moviedetail.PNG)
+View for the movie detail. Users can see the movie title, the movie poster, synopsis, and rating for the movie. Included also the return to home button.
--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-var movie_title = [
+const movie_title = [
     {
         id : '1',
         title : 'Avengers: Endgame',
         picture_url : './img/avengersendgame.jpg',
-        detail : 'After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.'
+        detail : 'After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.',
+        rating : '8.4/10 imdb'
     },
     {
         id : '2',
         title : 'Avengers: Infinity Wars',
         picture_url : './img/avengerinfinitywar.jpg',
-        detail : 'The Avengers must stop Thanos, an intergalactic warlord, from getting his hands on all the infinity stones. However, Thanos is prepared to go to any lengths to carry out his insane plan.'
+        detail : 'The Avengers must stop Thanos, an intergalactic warlord, from getting his hands on all the infinity stones. However, Thanos is prepared to go to any lengths to carry out his insane plan.',
+        rating : '8.4/10 imdb'
     },
     {
         id : '3',
         title : 'Venom 2: Let There Be Carnage',
         picture_url : './img/lettherebecarnage.jpg',
-        detail : 'Eddie Brock is still struggling to coexist with the shape-shifting extraterrestrial Venom. When deranged serial killer Cletus Kasady also becomes host to an alien symbiote, Brock and Venom must put aside their differences to stop his reign of terror.'
+        detail : 'Eddie Brock is still struggling to coexist with the shape-shifting extraterrestrial Venom. When deranged serial killer Cletus Kasady also becomes host to an alien symbiote, Brock and Venom must put aside their differences to stop his reign of terror.',
+        rating : '6.4/10 imdb'
     },
     {
         id : '4',
         title : 'John Wick',
         picture_url : './img/johnwick.jpg',
-        detail : `After the sudden death of his beloved wife, John Wick receives one last gift from her, a beagle puppy named Daisy, and a note imploring him not to forget how to love. But John's mourning is interrupted when his 1969 Boss Mustang catches the eye of sadistic thug Iosef Tarasov who breaks into his house and steals it, beating John unconscious and leaving Daisy dead. Unwittingly, they have just reawakened one of the most brutal assassins the underworld has ever seen.`
+        detail : `After the sudden death of his beloved wife, John Wick receives one last gift from her, a beagle puppy named Daisy, and a note imploring him not to forget how to love. But John's mourning is interrupted when his 1969 Boss Mustang catches the eye of sadistic thug Iosef Tarasov who breaks into his house and steals it, beating John unconscious and leaving Daisy dead. Unwittingly, they have just reawakened one of the most brutal assassins the underworld has ever seen.`,
+        rating : '7.4/10 imdb'
     }
 ]  
 
@@ -52,7 +56,8 @@ function show_movie_detail(){
     console.log(id);
     
     document.getElementById("movietitle").innerHTML = movie_title[id-1].title;
+    document.getElementById("rating").innerHTML = movie_title[id-1].rating;
     document.getElementById("moviedetail").innerHTML = "Sinopsis: "+movie_title[id-1].detail;
     document.getElementById("movieimage").src = movie_title[id-1].picture_url;
-    
+
 }
